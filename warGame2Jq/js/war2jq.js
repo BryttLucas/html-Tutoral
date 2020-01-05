b92548975832c2f01436ee98ff44dda55dbc2619
BryttLucas <workingB@fuse.net>
2020-01-04 21:34:56 -0500

    add

--- warGame2Jq/js/war2jq.js
+++ warGame2Jq/js/war2jq.js
@@ -4,44 +4,45 @@ let mypoints = gamePoints;
 let opppoints = gamePoints;
 let myNbr = 0;
 let oppNbr = 0;
-$().ready(() => {                
+
+$().ready(() => {
     display();
 
-    $("#getnbr").click(()=> {
-        myNbr = Number(Math.floor(Math.random() * 11)+1);
+    $("#getnbr").click(()=>{
+        myNbr = Number(Math.floor(Math.random() * 11));
         $("#mynbr").val(myNbr);
-        document.getElementById("mynbr").value = myNbr;
-
-    });
+    })
+    
     $("#winner").click(() => {
-     mypoints += pointValue;
-     opppoints -= pointValue;
-     reset();
-     display();
-    });
-
+        mypoints += pointValue;
+        opppoints -= pointValue;
+        reset();
+        display();
+    })
+    
     $("#loser").click(() => {
-    mypoints -= pointValue;
-    opppoints += pointValue;
-    reset();
-    display();
+        mypoints -= pointValue;
+        opppoints += pointValue;
+        reset();
+        display();
+    })
 
-    });
     $("#tie").click(() => {
-    pointValue = pointValue == 1 ? 5 : pointValue + 5;
-    $("#msg").css("display", "unset");
-    $("#tie").text("War!");
-    display();
-    });    
-});
+        pointValue = pointValue == 1 ? 5 : pointValue + 5;
+        $("#msg").css("display","unset");
+        $("#tie").text("War!");
+        display()
+    })
+})
+
 const display = () => {
-    $("#myscore").text (mypoints);
-    $("#oppscore").text (opppoints);
-    $("#ptvalue").text = (pointValue);
-    $("#mynbr").value('');
+    $("#myscore").text(mypoints);
+    $("#oppscore").text(opppoints);
+    $("#ptvalue").text(pointValue);
+    $("#mynbr").val('');
 }
 const reset = () => {
-    $("#msg").css("display","none");
-    $("#tie").css("Tie");
+    $("#msg").css("display", "none");
+    $("#tie").text("Tie");
     pointValue = 1;
 }
